407876072fc172a0ca3b66c76d497547fdf2edc4
Joel Gamonez <j.e.h.gamonez@gmail.com>
2025-04-26 15:35:25 +0200

    installed mysqlclient using pipenv

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'MySQLMasterPass123!',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
