3eb788ce736259ef289e980e88bed547e4427e5c
Joel Gamonez <j.e.h.gamonez@gmail.com>
2025-04-30 02:10:09 +0200

    Made some changes including model serializers

--- LittleLemon/restaurant/views.py
+++ LittleLemon/restaurant/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import Menu
+from .serializers import MenuSerializer
+from rest_framework import generics
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
