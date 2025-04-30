4d40f369dfe29d450406d844c0b6479b059bb7f5
Joel Gamonez <j.e.h.gamonez@gmail.com>
2025-04-30 17:00:38 +0200

    Made some changes to various modules to improve permissions and security of the app

--- LittleLemon/restaurant/views.py
+++ LittleLemon/restaurant/views.py
@@ -4,7 +4,7 @@ from .serializers import MenuSerializer, BookingSerializer
 from rest_framework import generics, viewsets
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
-from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 def index(request):
@@ -13,6 +13,7 @@ def index(request):
 class MenuItemView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
 
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
@@ -21,8 +22,9 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
 
 class UserViewSet(viewsets.ModelViewSet):
    queryset = User.objects.all()
    serializer_class = UserSerializer
-   permission_classes = [permissions.IsAuthenticated]
+   permission_classes = [IsAuthenticated]
